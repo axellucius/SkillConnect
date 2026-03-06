bcbe8d05a52ed8b59fc5992ebaf84ac037dbf707
VincentGenesius <vincentgtkj@gmail.com>
2026-03-06 14:16:23 +0700

    feat: formatting router

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace App\Core;
+
 use App\Controllers\StudentController;
 
 class Router
 {
-    private array $routes =[];
+    private array $routes = [];
 
     public function add(string $method, string $uri, string $controller, string $function)
     {
@@ -19,34 +21,32 @@ class Router
     public function run()
     {
         $method = $_SERVER['REQUEST_METHOD'];
-        $uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-        foreach ($this->routes as $route){
+        foreach ($this->routes as $route) {
             $pattern = str_replace(
-            '{id}',
-            '([0-9]+)',
-            $route['uri']
+                '{id}',
+                '([0-9]+)',
+                $route['uri']
             );
             $pattern = '#^' . $pattern . '$#';
 
-            if(preg_match($pattern, $uri, $matches)){
+            if (preg_match($pattern, $uri, $matches)) {
                 array_shift($matches);
 
-                require_once '../app/controllers/'. $route['controller'] . '.php';
+                require_once '../app/controllers/' . $route['controller'] . '.php';
 
                 $controllerClass = 'App\\Controllers\\' . $route['controller'];
                 $controller = new $controllerClass();
                 $function = $route['function'];
-                call_user_func_array([$controller,$function],$matches);
+                call_user_func_array([$controller, $function], $matches);
                 return;
             }
         }
-       
- 
- 
+
+
+
         http_response_code(404);
-         echo '<h1>404 - Page Not Found</h1>';
+        echo '<h1>404 - Page Not Found</h1>';
     }
 }
- 
-?>
