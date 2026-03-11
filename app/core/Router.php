2f3a77a74f5d2ddd4f7cc9f490bdcab5de66fe22
VincentGenesius <vincentgtkj@gmail.com>
2026-03-11 11:41:53 +0700

    wip: build register page

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Core;
 
 use App\Controllers\StudentController;
---
+++ app/views/auth/register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SkillConnect</title>
+    <link rel="stylesheet" href="/css/output.css">
+</head>
+<body>
+    <!-- IMAGE START -->
+    <img src="" alt="">
+    <!-- IMAGE END -->
+
+    <!-- MAIN START -->
+    <main>
+        <div>
+            <p class="">START YOUR SKILL JOURNEY</p>
+            <h1>Create your account!</h1>
+        </div>
+
+
+    </main>
+    <!-- MAIN END -->
+</body>
+</html>
