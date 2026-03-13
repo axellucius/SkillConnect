2231edff72cd94ad636fd846ea9db80e893d5646
VincentGenesius <vincentgtkj@gmail.com>
2026-03-13 11:41:16 +0700

    wip: build register page and add folder assets to public

--- app/views/auth/register.php
+++ app/views/auth/register.php
@@ -6,20 +6,26 @@
     <title>SkillConnect</title>
     <link rel="stylesheet" href="/css/output.css">
 </head>
-<body>
-    <!-- IMAGE START -->
-    <img src="" alt="">
-    <!-- IMAGE END -->
-
-    <!-- MAIN START -->
-    <main>
-        <div>
-            <p class="">START YOUR SKILL JOURNEY</p>
-            <h1>Create your account!</h1>
+<body class="bg-gray-100 flex items-center justify-center min-h-screen">
+    <div class="flex gap-8 bg-white py-4 px-12">
+        <!-- IMAGE START -->
+        <div class="w-[440px]">
+            <img src="/assets/images/register.png" class="w-full h-full object-cover" alt="register-image">
         </div>
-
-
-    </main>
-    <!-- MAIN END -->
+        <!-- IMAGE END -->
+    
+        <!-- MAIN START -->
+        <main class="flex flex-col gap-6">
+            <div>
+                <p class="text-xl">START YOUR SKILL JOURNEY</p>
+                <h1 class="font-bold text-4xl">Create your account!</h1>
+            </div>
+    
+            <form action="  ">
+                <input type="text" placeholder="Username" class="w-full bg-black text-white p-3 rounded-lg">
+            </form>
+        </main>
+        <!-- MAIN END -->
+    </div>
 </body>
 </html>
