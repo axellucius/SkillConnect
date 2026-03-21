a7aefcf374bf7edef3a324dc3e8b6618329f5659
VincentGenesius <vincentgtkj@gmail.com>
2026-03-21 22:07:48 +0700

    wip: add and build list-skills and detail-skill pages

--- public/index.php
+++ public/index.php
@@ -9,6 +9,8 @@ $router->add("GET", "/login", "AuthController", "login");
 $router->add("GET", "/register", "AuthController", "register");
 $router->add("GET", "/profile", "AuthController", "profile");
 $router->add("GET", "/skills", "AuthController", "skills");
+$router->add("GET", "/list-skills", "AuthController", "listSkills");
+$router->add("GET", "/skill-detail", "AuthController", "skillDetail");
 
 
 $router->run();
