4865a5486a82b2cfc3838ab06000df9846856f5b
VincentGenesius <vincentgtkj@gmail.com>
2026-04-27 15:28:02 +0700

    feat: implement popular skills in home page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -15,7 +15,7 @@
 
     <section class="grid grid-cols-1 md:grid-cols-2 gap-8">
         <!-- CARD 1 START -->
-        <article class="group flex flex-col border border-gray-300 overflow-hidden hover:translate-y-1.5 transition-all h-full bg-white">
+        <article class="group flex flex-col border border-gray-300 overflow-hidden hover:translate-y-1.5 transition-all duration-500 ease-in-out h-full bg-white">
             <div class="relative w-full h-60 overflow-hidden after:content-[''] after:absolute after:inset-0 after:bg-gradient-to-t after:from-[#EF6C32] after:to-transparent after:opacity-0 group-hover:after:opacity-100 after:transition-opacity after:duration-300">
                 <img src="/assets/images/motto-1.png" class="w-full h-full object-cover" alt="Expert Students">
             </div>
@@ -30,7 +30,7 @@
         <!-- CARD 1 END -->
 
         <!-- CARD 2 START -->
-        <article class="group flex flex-col border border-gray-300 overflow-hidden hover:translate-y-1.5 transition-all h-full bg-white">
+        <article class="group flex flex-col border border-gray-300 overflow-hidden hover:translate-y-1.5 transition-all duration-500 ease-in-out h-full bg-white">
             <div class="relative w-full h-60 overflow-hidden after:content-[''] after:absolute after:inset-0 after:bg-gradient-to-t after:from-[#EF6C32] after:to-transparent after:opacity-0 group-hover:after:opacity-100 after:transition-opacity after:duration-300">
                 <img src="/assets/images/motto-2.png" class="w-full h-full object-cover" alt="Peer Learning">
             </div>
@@ -54,17 +54,58 @@
             <h1 class="font-bold text-3xl">Explore Popular Skills</h1>
         </div>
 
-        <a href="#" class="text-white text-md font-semibold px-6 py-3 rounded-lg bg-[#2A2F5A] hover:translate-y-1 transition-all">See More</a>
+        <a href="#" class="text-white text-md font-semibold px-6 py-3 rounded-lg bg-[#2A2F5A] hover:translate-y-1 hover:bg-[#454b85] transition-all">See More</a>
     </div>
 
-    <article class="text-white">
-        <div class="border border-gray-500">
-            <img src="" alt="">
-            <h1>UI/UX Design</h1>
-            <p>Designing user interfaces that are visually appealing and easy to use. Focuses on creating a smooth and enjoyable user experience.</p>
+    <article class="text-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            <img src="/assets/images/ui-ux.png" class="w-10 pb-2" alt="UI/UX Design">
+            <h1 class="font-bold text-2xl leading-7">UI/UX <br> Design</h1>
+            <p class="font-light text-sm leading-5 pt-3">Designing user interfaces that are visually appealing and easy to use. Focuses on creating a smooth and enjoyable user experience.</p>
         </div>
-        
-        
+
+        <div class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            <img src="/assets/images/web-dev.png" class="w-10 pb-2" alt="UI/UX Design">
+            <h1 class="font-bold text-2xl leading-7">Web <br> Development</h1>
+            <p class="font-light text-sm leading-5 pt-3">Building and maintaining websites using various technologies. Includes both frontend and backend development.</p>
+        </div>
+
+        <div class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            <img src="/assets/images/ui-ux.png" class="w-10 pb-2" alt="UI/UX Design">
+            <h1 class="font-bold text-2xl leading-7">Digital <br> Marketing</h1>
+            <p class="font-light text-sm leading-5 pt-3">Promoting products or services through online platforms and strategies. Helps increase visibility and reach a wider audience.</p>
+        </div>
+
+        <div class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            <img src="/assets/images/ui-ux.png" class="w-10 pb-2" alt="UI/UX Design">
+            <h1 class="font-bold text-2xl leading-7">Project <br> Management</h1>
+            <p class="font-light text-sm leading-5 pt-3">Planning, organizing, and managing tasks to achieve project goals. Ensures work is completed efficiently and on time.</p>
+        </div>
+
+        <div class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            <img src="/assets/images/ui-ux.png" class="w-10 pb-2" alt="UI/UX Design">
+            <h1 class="font-bold text-2xl leading-7">Video <br> Editing</h1>
+            <p class="font-light text-sm leading-5 pt-3">Editing and arranging video content to create engaging visuals. Involves cutting, effects, and improving overall quality.</p>
+        </div>
+
+        <div class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            <img src="/assets/images/ui-ux.png" class="w-10 pb-2" alt="UI/UX Design">
+            <h1 class="font-bold text-2xl leading-7">Motion <br> Design</h1>
+            <p class="font-light text-sm leading-5 pt-3">Creating animated graphics and visual effects for digital content. Enhances storytelling through movement and transitions.</p>
+        </div>
+
+        <div class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            <img src="/assets/images/ui-ux.png" class="w-10 pb-2" alt="UI/UX Design">
+            <h1 class="font-bold text-2xl leading-7">Graphic <br> Design</h1>
+            <p class="font-light text-sm leading-5 pt-3">Creating visual content such as posters, logos, and branding materials. Focuses on layout, color, and visual communication.</p>
+        </div>
+
+        <div class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            <img src="/assets/images/ui-ux.png" class="w-10 pb-2" alt="UI/UX Design">
+            <h1 class="font-bold text-2xl leading-7">Public <br> Speaking</h1>
+            <p class="font-light text-sm leading-5 pt-3">Delivering ideas clearly and confidently in front of an audience. Helps improve communication and presentation skills.</p>
+        </div>
+
     </article>
 </section>
 <!-- POPULAR SKILLS END -->
