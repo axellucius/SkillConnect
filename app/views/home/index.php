480795919752d2ca0c66b62e7c6cbc157f8792c6
VincentGenesius <vincentgtkj@gmail.com>
2026-04-28 21:13:00 +0700

    feat: implement testimonials in home page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,9 +1,9 @@
 <!-- SLIDESHOW START -->
-<!-- <section>
+<section>
     <div>
-
+        
     </div>
-</section> -->
+</section>
 <!-- SLIDESHOW END -->
 
 <!-- MOTTO START -->
@@ -16,7 +16,7 @@
     <section class="grid grid-cols-1 md:grid-cols-2 gap-8">
         <!-- CARD 1 START -->
         <article
-            class="group flex flex-col border border-gray-300 overflow-hidden hover:translate-y-1.5 transition-all duration-500 ease-in-out h-full bg-white">
+            class="group flex flex-col border border-gray-300 overflow-hidden hover:-translate-y-1.5 transition-all duration-500 ease-in-out h-full bg-white">
             <div
                 class="relative w-full h-60 overflow-hidden after:content-[''] after:absolute after:inset-0 after:bg-linear-to-t after:from-[#EF6C32] after:to-transparent after:opacity-0 group-hover:after:opacity-100 after:transition-opacity after:duration-300">
                 <img src="/assets/images/motto-1.png" class="w-full h-full object-cover" alt="Expert Students">
@@ -35,7 +35,7 @@
 
         <!-- CARD 2 START -->
         <article
-            class="group flex flex-col border border-gray-300 overflow-hidden hover:translate-y-1.5 transition-all duration-500 ease-in-out h-full bg-white">
+            class="group flex flex-col border border-gray-300 overflow-hidden hover:-translate-y-1.5 transition-all duration-500 ease-in-out h-full bg-white">
             <div
                 class="relative w-full h-60 overflow-hidden after:content-[''] after:absolute after:inset-0 after:bg-linear-to-t after:from-[#EF6C32] after:to-transparent after:opacity-0 group-hover:after:opacity-100 after:transition-opacity after:duration-300">
                 <img src="/assets/images/motto-2.png" class="w-full h-full object-cover" alt="Peer Learning">
@@ -63,13 +63,13 @@
         </div>
 
         <a href="#"
-            class="text-white text-md font-semibold px-6 py-3 rounded-lg bg-[#2A2F5A] hover:translate-y-1 hover:bg-[#454b85] transition-all">See
+            class="text-white text-md font-semibold px-6 py-3 rounded-lg bg-[#2A2F5A] hover:-translate-y-1 hover:bg-[#454b85] transition-all">See
             More</a>
     </div>
 
     <article class="text-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         <div
-            class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            class="group border border-gray-500 rounded-sm p-4 hover:-translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
             <img src="/assets/images/ui-ux.png" class="w-10 pb-2" alt="UI/UX Design">
             <h1 class="font-bold text-2xl leading-7">UI/UX <br> Design</h1>
             <p class="font-light text-sm leading-5 pt-3">Designing user interfaces that are visually appealing and easy
@@ -77,7 +77,7 @@
         </div>
 
         <div
-            class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            class="group border border-gray-500 rounded-sm p-4 hover:-translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
             <img src="/assets/images/web-dev.png" class="w-10 pb-2" alt="UI/UX Design">
             <h1 class="font-bold text-2xl leading-7">Web <br> Development</h1>
             <p class="font-light text-sm leading-5 pt-3">Building and maintaining websites using various technologies.
@@ -85,7 +85,7 @@
         </div>
 
         <div
-            class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            class="group border border-gray-500 rounded-sm p-4 hover:-translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
             <img src="/assets/images/digital-marketing.png" class="w-10 pb-2" alt="UI/UX Design">
             <h1 class="font-bold text-2xl leading-7">Digital <br> Marketing</h1>
             <p class="font-light text-sm leading-5 pt-3">Promoting products or services through online platforms and
@@ -93,7 +93,7 @@
         </div>
 
         <div
-            class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            class="group border border-gray-500 rounded-sm p-4 hover:-translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
             <img src="/assets/images/project-management.png" class="w-10 pb-2" alt="UI/UX Design">
             <h1 class="font-bold text-2xl leading-7">Project <br> Management</h1>
             <p class="font-light text-sm leading-5 pt-3">Planning, organizing, and managing tasks to achieve project
@@ -101,7 +101,7 @@
         </div>
 
         <div
-            class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            class="group border border-gray-500 rounded-sm p-4 hover:-translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
             <img src="/assets/images/video-editing.png" class="w-10 pb-2" alt="UI/UX Design">
             <h1 class="font-bold text-2xl leading-7">Video <br> Editing</h1>
             <p class="font-light text-sm leading-5 pt-3">Editing and arranging video content to create engaging visuals.
@@ -109,7 +109,7 @@
         </div>
 
         <div
-            class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            class="group border border-gray-500 rounded-sm p-4 hover:-translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
             <img src="/assets/images/motion-design.png" class="w-10 pb-2" alt="UI/UX Design">
             <h1 class="font-bold text-2xl leading-7">Motion <br> Design</h1>
             <p class="font-light text-sm leading-5 pt-3">Creating animated graphics and visual effects for digital
@@ -117,7 +117,7 @@
         </div>
 
         <div
-            class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            class="group border border-gray-500 rounded-sm p-4 hover:-translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
             <img src="/assets/images/graphic-design.png" class="w-10 pb-2" alt="UI/UX Design">
             <h1 class="font-bold text-2xl leading-7">Graphic <br> Design</h1>
             <p class="font-light text-sm leading-5 pt-3">Creating visual content such as posters, logos, and branding
@@ -125,7 +125,7 @@
         </div>
 
         <div
-            class="group border border-gray-500 rounded-sm p-4 hover:translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
+            class="group border border-gray-500 rounded-sm p-4 hover:-translate-y-2 hover:bg-[#ef6c32] transition-all duration-500 ease-in-out cursor-pointer">
             <img src="/assets/images/public-speaking.png" class="w-10 pb-2" alt="UI/UX Design">
             <h1 class="font-bold text-2xl leading-7">Public <br> Speaking</h1>
             <p class="font-light text-sm leading-5 pt-3">Delivering ideas clearly and confidently in front of an
@@ -147,7 +147,7 @@
         <div class="flex flex-col w-106 p-6 rounded-lg bg-linear-to-b from-[#292D4A] to-[#000000] text-white">
             <h1 class="font-semibold text-xl">Still Have A Questions?</h1>
             <p class="font-light text-sm mb-4">We're Here To Help You Find The Answers You Need.</p>
-            <a href="#" class="flex text-white text-md font-semibold w-36 justify-center py-2 rounded-lg bg-[#2A2F5A] hover:translate-y-1 hover:bg-[#454b85] transition-all">Send Email</a>
+            <a href="#" class="flex text-white text-md font-semibold w-36 justify-center py-2 rounded-lg bg-[#2A2F5A] hover:-translate-y-1 hover:bg-[#454b85] transition-all">Send Email</a>
         </div>
     </article>
 
@@ -216,7 +216,7 @@
 <!-- SUPPORT FAQ END -->
 
 <!-- TESTIMONIALS START -->
-<section class="ml-12 mb-10 flex items-center">
+<section class="ml-12 mb-10 mt-5 flex items-center">
     <article class="border border-[#333333] rounded-lg py-12 pl-8 pr-30">
         <p class="text-[#333333] font-light text-xl tracking-widest">TESTIMONIALS</p>
         <h1 class="text-4xl font-bold text-[#333333] tracking-tight">What Students Say</h1>
@@ -244,13 +244,13 @@
                 <path d="M14.017 21L14.017 18C14.017 16.8954 14.9124 16 16.017 16H19.017C19.5693 16 20.017 15.5523 20.017 15V9C20.017 8.44772 19.5693 8 19.017 8H15.017C14.4647 8 14.017 8.44772 14.017 9V11C14.017 11.5523 13.5693 12 13.017 12H12.017C11.4647 12 11.017 11.5523 11.017 11V9C11.017 6.79086 12.8079 5 15.017 5H19.017C21.2261 5 23.017 6.79086 23.017 9V15C23.017 18.3137 20.3307 21 17.017 21H14.017ZM1.017 21L1.017 18C1.017 16.8954 1.91243 16 3.017 16H6.017C6.56928 16 7.017 15.5523 7.017 15V9C7.017 8.44772 6.56928 8 6.017 8H2.017C1.46472 8 1.017 8.44772 1.017 9V11C1.017 11.5523 0.569282 12 0.017 12H-0.983C-1.53528 12 -2.017 11.5523 -2.017 11V9C-2.017 6.79086 -0.226142 5 1.983 5H6.017C8.22614 5 10.017 6.79086 10.017 9V15C10.017 18.3137 7.33014 21 4.017 21H1.017Z" />
             </svg>
             
-            <p class="text-sm font-light leading-relaxed mb-2 text-gray-300">SkillConnect helped me find friends with the same interests and work on projects together. It made learning more fun and easier.</p>
+            <p class="text-sm font-light leading-relaxed mb-2 text-gray-300">I was able to improve my skills by collaborating with other students. The platform is simple and really helpful.</p>
 
             <div class="border-t border-gray-700 pt-5 flex items-center gap-3">
                 <div class="w-10 h-10 rounded-full overflow-hidden">
-                    <img src="/assets/images/photo-profile-2.jpg" alt="User">
+                    <img src="/assets/images/photo-profile-3.jpg" alt="User">
                 </div>
-                <p class="font-semibold text-sm">Claryn Grazelyn</p>
+                <p class="font-semibold text-sm">Carlos Aprilio</p>
             </div>
         </article>
     </div>
