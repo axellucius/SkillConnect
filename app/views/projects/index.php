c963bebf0b33cd5f14dd12f91638f721673a068d
VincentGenesius <vincentgtkj@gmail.com>
2026-05-09 22:36:50 +0700

    wip: build projects page

--- app/views/projects/index.php
+++ app/views/projects/index.php
@@ -14,14 +14,14 @@
         </div>
 
         <div class="flex gap-2">
-            <button class="bg-[#E01014] p-2 rounded-md">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-white">
+            <button class="bg-[#E01014] p-3 rounded-md hover:bg-[#ff2e31] transition-all">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 text-white">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>
             </button>
     
-            <button class="bg-[#2A2F5A] p-2 rounded-md">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-white">
+            <button class="bg-[#2A2F5A] p-3 rounded-md hover:bg-[#323b91] transition-all">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 text-white">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
             </button>
@@ -38,15 +38,21 @@
                     <img src="#" class="w-8 h-8 rounded-full object-cover">
                 </div>
 
-                <h2 class="font-semibold text-[#333333] text-lg">Melodix: Immersive Audio Player</h2>
+                <h2 class="font-bold text-[#333333] text-xl mb-3">Melodix: Immersive Audio Player</h2>
 
-                <div class="flex items-center gap-1 text-xs text-gray-500 mb-2">
-                    <div>
-                        <p>VibeLogic Team</p>
+                <div class="flex flex-col gap-1 text-xs text-gray-500 pb-3 border-b border-[#d9d9d9]">
+                    <div class="flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5 text-[#333333]">
+                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                        </svg>
+                        <p class="text-[#333333]">VibeLogic Team</p>
                     </div>
 
-                    <div>
-                        <p></p>
+                    <div class="flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5 text-[#333333]">
+                            <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
+                        </svg>
+                        <p class="text-[#333333]">4 Members</p>
                     </div>
                 </div>
             </div>
@@ -57,24 +63,6 @@
                     <img src="" class="w-7 h-7 rounded-full border-2 border-white object-cover">
                     <div class="w-7 h-7 rounded-full bg-gray-200 border-2 border-white flex items-center justify-center text-xs text-gray-500"></div>
                 </div>
-            </div>
-
-            <!-- Action Buttons -->
-            <div class="flex justify-between items-center mt-4 pt-3 border-t border-gray-100">
-                <button onclick="openEdit()" class="text-xs text-blue-500 hover:underline">Edit</button>
-                <form method="POST">
-                    <input type="hidden" name="action" value="delete">
-                    <input type="hidden" name="project_id" value="">
-                    <button onclick="return confirm('Hapus project ini?')" class="text-xs text-red-400 hover:underline">Hapus</button>
-                </form>
-
-                <form method="POST">
-                    <input type="hidden" name="action" value="join">
-                    <input type="hidden" name="project_id" value="">
-                    <button class="text-xs text-green-500 hover:underline">Gabung</button>
-                </form>
-
-                <span class="text-xs text-gray-400">✓ Member</span>
             </div>
         </div>
         <!-- PROJECT CARD END -->
