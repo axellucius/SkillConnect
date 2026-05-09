0cac9d1a974a515234816a005b69df803373dff2
VincentGenesius <vincentgtkj@gmail.com>
2026-05-09 18:23:17 +0700

    wip: build projects page

--- app/views/projects/index.php
+++ app/views/projects/index.php
@@ -13,57 +13,49 @@
             <p class="px-2 border rounded-sm">2</p>
         </div>
 
-        <button class="bg-[#E8EFF7] p-2 rounded-md">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-            </svg>
-        </button>
+        <div class="flex gap-2">
+            <button class="bg-[#E01014] p-2 rounded-md">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-white">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                </svg>
+            </button>
+    
+            <button class="bg-[#2A2F5A] p-2 rounded-md">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-white">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+            </button>
+        </div>
     </article>
     <!-- PROJECT HEADER END -->
 
     <!-- PROJECT LIST START -->
     <article class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mx-12 mt-4">
+        <!-- PROJECT CARD START -->
         <div class="bg-white rounded-xl border border-gray-100 shadow-sm p-4 flex flex-col justify-between">
             <div>
                 <div class="w-12 h-12 bg-gray-900 rounded-full flex items-center justify-center mb-3">
-                    <img src="" class="w-8 h-8 rounded-full object-cover">
-                    <!-- SVG default icon -->
-                    <svg class="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 24 24">
-                        <path d="M12 2a10 10 0 100 20A10 10 0 0012 2z" />
-                    </svg>
+                    <img src="#" class="w-8 h-8 rounded-full object-cover">
                 </div>
 
-                <h2 class="font-semibold text-gray-900 text-sm mb-2"></h2>
-
-                <div class="flex items-center gap-1 text-xs text-gray-500 mb-1">
-                    <svg class="w-4 h-4"></svg>
-                    <span></span>
-                </div>
-
-                <!-- Deadline -->
+                <h2 class="font-semibold text-[#333333] text-lg">Melodix: Immersive Audio Player</h2>
 
                 <div class="flex items-center gap-1 text-xs text-gray-500 mb-2">
-                    <svg class="w-4 h-4"></svg>
-                    <span></span>
-                </div>
-
-            </div>
-
-            <!-- Avatar Member -->
-            <div class="mt-3">
-                <p class="text-xs text-gray-400 mb-1">Team Member</p>
-                <div class="flex items-center -space-x-2">
-
-                    <img src=""
-                        title=""
-                        class="w-7 h-7 rounded-full border-2 border-white object-cover">
-
-                    <div class="w-7 h-7 rounded-full bg-gray-200 border-2 border-white
-                        flex items-center justify-center text-xs text-gray-500">
-
+                    <div>
+                        <p>VibeLogic Team</p>
                     </div>
 
+                    <div>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-3">
+                <p class="text-xs text-[#333333] mb-1">Team Member</p>
+                <div class="flex items-center -space-x-2">
+                    <img src="" class="w-7 h-7 rounded-full border-2 border-white object-cover">
+                    <div class="w-7 h-7 rounded-full bg-gray-200 border-2 border-white flex items-center justify-center text-xs text-gray-500"></div>
                 </div>
             </div>
 
@@ -85,6 +77,7 @@
                 <span class="text-xs text-gray-400">✓ Member</span>
             </div>
         </div>
+        <!-- PROJECT CARD END -->
 
         <!-- <div class="col-span-4 text-center text-gray-400 py-16">
             <p>Belum ada project. Klik <strong>+</strong> untuk membuat yang pertama!</p>
